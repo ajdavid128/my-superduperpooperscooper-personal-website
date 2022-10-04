0097f94af7d49c3f420dc41aed0c32481c9cb0df
Aaron David <ajdavid128@gmail.com>
2022-10-04 11:09:48 -0500

    customized buttons with css, changed color palette of website, tested out different Google fonts and how to upload effects through Google Fonts API

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ h3.textContent = "Are you ready to get to know a little bit about Aaron?";
 document.querySelector("body").appendChild(h3);
 
 const h5 = document.createElement("h5");
-h5.textContent = "…Because you’re about too!";
+h5.textContent = "…Because you’re about to!";
 document.querySelector("body").appendChild(h5);
 
 
