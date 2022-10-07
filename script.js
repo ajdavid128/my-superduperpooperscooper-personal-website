8a2eb24cc727f74b9548119ae164fa020ffed80f
Aaron David <ajdavid128@gmail.com>
2022-10-07 13:10:52 -0500

    tried inserting the color blocks for the background with css, added html div sections to build out color blocks, still trying to understand the padding/magin/content/border attributes to better refine content shaping

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const h3 = document.createElement("h3");
 h3.textContent = "Are you ready to get to know a little bit about Aaron?";
-document.querySelector("body").appendChild(h3);
+document.querySelector("div.frame2").appendChild(h3);
 
 const h5 = document.createElement("h5");
 h5.textContent = "…Because you’re about to!";
-document.querySelector("body").appendChild(h5);
+document.querySelector("div.frame2").appendChild(h5);
 
 
